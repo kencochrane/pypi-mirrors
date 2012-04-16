c5b1f96220f2e081f47ae34987f343c240bd2aac
Ken Cochrane <kencochrane@gmail.com>
2012-04-15 21:21:01 -0400

    added ability to look up the location of the mirror by using API

---
+++ iploc.py
@@ -0,0 +1,18 @@
+import json, urllib, urllib2
+
+
+def get_city(apikey, ip):
+    """ get city location for an ip """
+    base_url = "http://api.ipinfodb.com/v2/ip_query.php"
+    variables = {"output":"json",
+                "timezone":'false',
+                "key":apikey,
+                "ip":ip,}
+
+    urldata = urllib.urlencode(variables)
+    url = "{0}?{1}".format(base_url, urldata)
+    urlobj = urllib2.urlopen(url)
+    data = urlobj.read()
+    urlobj.close()
+    return json.loads(data)
+    
