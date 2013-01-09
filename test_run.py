9b082eac4b2c89a8401de3f77edcb73c320082bc
Ken Cochrane <KenCochrane@gmail.com>
2013-01-09 11:08:20 -0500

    fixed a bug that was causing an issue when e.pypi.python.org wasn't available

--- test_run.py
+++ test_run.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python
 
+from jinja2 import Environment, FileSystemLoader
 from pypimirrors import mirror_statuses
 
-from utils import (cache_key, location_name, get_total_seconds, 
-                   get_connection, store_page_data, find_number_of_packages,
-                   get_location_for_mirror)
+from utils import (location_name, get_total_seconds, find_number_of_packages)
 
 from config import UNOFFICIAL_MIRRORS
 
 def process_results(results):
     """ process the results and gather data """
 
-    conn = get_connection()
     new_results = []
     for d in results:
         mirror = d.get('mirror')
         status = d.get('status')
-        location = get_location_for_mirror(mirror)
-        d['location'] = location_name(location)
+        d['location'] = "n/a"
         if  status != 'Unavailable':
             resp_time = d.get('response_time')
             age = get_total_seconds(d.get('time_diff'))
-            conn.rpush(cache_key('RESPTIME', mirror), resp_time )
-            conn.rpush(cache_key('AGE', mirror), age)
-            resp_list = conn.lrange(cache_key('RESPTIME', mirror), -60, -1)
-            age_list = conn.lrange(cache_key('AGE', mirror), -60, -1)
+            resp_list = ["1","2","3","4","5","6","7","8","9","10"] # faked out for test
+            age_list = ["1","2","3","4","5","6","7","8","9","10"] # faked out for test
             d['num_packages'] = find_number_of_packages(mirror)
             d['resp_list'] = ",".join(resp_list)
             d['age_list'] = ",".join(age_list)
@@ -39,7 +34,10 @@ def run():
         time_now = results[0].get('time_now', None)
     data = process_results(results)
 
-    store_page_data(data, time_now)
+    env = Environment(loader= FileSystemLoader('templates'))
+    template = env.get_template('index.html')
+    context = {'data': data, 'date_now': time_now}
+    print template.render(**context)
 
 if __name__ == '__main__':
     run()
